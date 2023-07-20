faf650cf466dd1a19ff88e0f9e9057516d279094
Dani <aulia.ram@gmail.com>
2023-07-20 08:15:33 +0000

    DAG

---
+++ airflow/dags/ingest_data.py
@@ -0,0 +1,117 @@
+import os
+import logging
+import gzip
+import io
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+
+from google.cloud import storage
+import pyarrow as pa
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+import pandas as pd
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+dataset_file = 'listings.csv.gz'
+dataset_url = f"http://data.insideairbnb.com/thailand/central-thailand/bangkok/2023-06-26/data/{dataset_file}"
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+parquet_file = dataset_file.replace('.csv.gz', '.parquet')
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'airbnb_all')
+
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv.gz'):
+        logging.error("Can only accept source files in csv.gz format, for the moment")
+        return
+    data = []
+    with gzip.open(src_file, 'rb') as f:
+        for line in f:
+            data.append(line)
+    df = pd.read_csv(io.BytesIO(b''.join(data)))
+    pq.write_table(pa.Table.from_pandas(df), src_file.replace('.csv.gz', '.parquet'))
+
+    
+# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="data_ingest",
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['airbnb'],
+) as dag:
+
+    start_task= DummyOperator(
+        task_id="start_task"
+    )
+
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"curl -sSLf {dataset_url} > {path_to_local_home}/{dataset_file}"
+    )
+
+    format_to_parquet_task = PythonOperator(
+        task_id="format_to_parquet_task",
+        python_callable=format_to_parquet,
+        op_kwargs={
+            "src_file": f"{path_to_local_home}/{dataset_file}",
+        },
+    )
+
+    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{parquet_file}",
+            "local_file": f"{path_to_local_home}/{parquet_file}",
+        },
+    )
+
+    rm_task = BashOperator(
+            task_id="rm_task",
+            bash_command=f"rm {path_to_local_home}/{dataset_file} {path_to_local_home}/{parquet_file}"
+    )
+
+    end_task= DummyOperator(
+        task_id="end_task"
+    )
+
+    start_task >> download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> rm_task >> end_task
